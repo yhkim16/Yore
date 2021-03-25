3d8e2b99d895b7a735b40f26a5b38fc5d085c146
yhkim16 <81220399+yhkim16@users.noreply.github.com>
2021-03-25 15:28:08 +0900

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,18 @@
 var express = require('express');
 var nunjucks = require('nunjucks');
 var app = express();
+
+var port = 3000;
+nunjucks.configure('.', {
+    autoescape: true,
+    express: app
+});
+
+app.use(express.json());
+app.use(express.urlencoded({
+    extended: true
+}));
+
+app.listen(port);
+
+
