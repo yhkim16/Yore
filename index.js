2f012d07e6d43ffae3b226d6341a28b1cf946e3e
yhkim16 <81220399+yhkim16@users.noreply.github.com>
2021-04-13 18:29:33 +0900

    select2 적용
    select2 라이브러리를 이용한 재료 목록 추가하기

--- index.js
+++ index.js
@@ -19,8 +19,8 @@ app.use('/', express.static(__dirname + '/www')); // redirect root
 app.use('/js', express.static(__dirname + '/node_modules/bootstrap/dist/js')); // redirect bootstrap JS 
 app.use('/js', express.static(__dirname + '/node_modules/jquery/dist')); // redirect JS jQuery 
 app.use('/css', express.static(__dirname + '/node_modules/bootstrap/dist/css')); // redirect CSS bootstrap
-
-
+app.use('/js', express.static(__dirname + '/node_modules/select2/dist/js')); // redirect select2 JS
+app.use('/css', express.static(__dirname + '/node_modules/select2/dist/css')); // redirect select2 css
 
 app.get('/', function (req, res) {
     res.render('main.html');
